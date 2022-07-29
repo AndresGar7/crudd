9126fde71c5178828feee0953d679653b893a389
TurismCol <eddy_gc07@hotmail.com>
2022-07-29 17:30:02 +0200

    migraciones realizadas

---
+++ database/migrations/2022_07_29_152116_create_club_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClubTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('club', function (Blueprint $table) {
+            $table->id();
+            $table->string('equipo')->unique();
+            $table->string('liga');
+            // $table->enum('liga',['ing','esp','ita','ale','fra']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('club');
+    }
+}
